67735917fc7234dabe3f694b5d8c6dfac40372d5
Abraham <abrahamalfonzo11gmail.com>
2024-07-25 20:32:05 -0400

    estilos aplicados

--- My-Portfolio/src/App.jsx
+++ My-Portfolio/src/App.jsx
@@ -1,103 +1,95 @@
 /* eslint-disable no-useless-escape */
 
-import { useEffect, useState } from 'react'
-import './App.css'
-import { Projects } from './Components/Projects'
-import { Tecnologias } from './Components/Tecnologias'
-import result from "./json/tecnologia.json";
+import './App.css';
+import { Projects } from './Components/Projects';
+import { Tecnologias } from './Components/Tecnologias';
+import result from './json/tecnologia.json';
+import response from './json/proyectos.json';
 export function App() {
   const projectObject = result.project;
-  console.log(projectObject);
-  const iconObject = [
-    {
-      name : 'JavaScript',
-      svg : 'svg/file_type_js_official_icon_130509.svg',
-      type: 'Frontend'
-    },{
-      name : 'Css',
-      svg : 'svg/file_type_css_icon_130661.svg',
-      type: 'Frontend'
-      
-    },{
-      name : 'HTML',
-      svg : 'svg/file_type_html_icon_130541.svg',
-      type: 'Frontend'
-    },{
-      name: 'Python',
-      svg : 'svg/file_type_python_icon_130221.svg',
-      type: 'Backend'
-    },{
-      name: 'React',
-      svg : 'svg/react_original_logo_icon_146374.svg',
-      type: 'Frontend'
-    },{
-      name: 'Git',
-      svg : 'svg/file_type_git_icon_130581.svg',
-      type: 'Herramientas'
-    },{
-      name: 'GitHub',
-      svg : 'svg/github_git_icon_145985.svg',
-      type: 'Herramientas'
-    },{
-      name: 'NodeJs',
-      svg : 'svg/file_type_node_icon_130301.svg',
-      type: 'Backend'
-    },{
-      name: 'VSC',
-      svg : 'svg/file_type_vscode_icon_130084.svg',
-      type: 'Herramientas'
-    },{
-      name: 'Vite',
-      svg : 'svg/vite_logo_icon_249258.svg',
-      type: 'Herramientas'
-    },{
-      name: 'React Native',
-      svg : 'svg/react_icon_212902.svg',
-      type: 'Aprendiendo'
-    },{
-      name: 'AstroJs',
-      svg : 'svg/astro_logo_icon_247543.svg',
-      type: 'Aprendiendo'
-    },{
-      name: 'MySql',
-      svg : 'svg/mysql_original_wordmark_logo_icon_146417.svg',
-      type: 'Aprendiendo'
-    },{
-      name: 'Flask',
-      svg: 'svg/pocoo_flask_logo_icon_168045.svg',
-      type: 'Backend'
-    },{
-      name: 'Npm',
-      svg: 'svg/npm_original_wordmark_logo_icon_146402.svg',
-      type: 'Herramientas'
-    },{
-      name: 'Tailwind',
-      svg: 'svg/file_type_tailwind_icon_130128.svg',
-      type: 'Aprendiendo'
-    }
 
-  ]
+  const iconObject = response.iconos;
   return (
     <>
-  <header className='cont_header-padre'>
-    <a href='#about'>SOBRE MI</a>
-    <a href='#projects'>PROYECTOS</a>
-    <a href='#tecnologias'>TECNOLOGÍAS</a>
-  </header>
-    <article className='cont_app-padre'>
-      <section className='presentacion'>
-      <h1>Abraham Alfonzo, Full Stack Developers</h1>
-      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Twemoji_1f600.svg/1200px-Twemoji_1f600.svg.png" alt="Abraham Alfonzo" className='yo_img'/>
-      </section>
-      <section className='cont_proyectos-padre'>
-          <h2>PROYECTOS</h2>
-          <Projects projectObject={projectObject}/>
-      </section>
-        <h2>Tecnologías</h2>
-        <Tecnologias iconObject={iconObject}/>
-    </article>
+      <header className="cont_header-padre">
+        <a href="#about">SOBRE MI</a>
+        <a href="#projects">PROYECTOS</a>
+        <a href="#tecnologias">TECNOLOGÍAS</a>
+      </header>
+      <article className="cont_app-padre">
+        <section className="presentacion">
+          <div className="cont_img">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Twemoji_1f600.svg/1200px-Twemoji_1f600.svg.png"
+              alt="Abraham Alfonzo"
+              className="yo_img"
+            />
+            <a href="">Disponible para trabajar</a>
+          </div>
+          <br />
+          <h1 id="about">Hey, soy Abraham</h1>
+          <br />
+          <p>
+            Desarrollador web fullstack ,{' '}
+            <span className="span_yellow">
+              estudiante de Ingienieria Informática{' '}
+            </span>
+            de Caracas, VEN. Especializado en el{' '}
+            <span className="span_yellow">
+              desarrollo de aplicaciones web únicas.
+            </span>
+          </p>
+          <br />
+          <br />
+          <div>
+            <a href="" className="a-cards">
+              Contáctame
+            </a>
+            <a href="" className="a-cards">
+              LinkedIn
+            </a>
+          </div>
+        </section>
+        <br />
+        <section className="cont_proyectos-padre">
+          <h2 id="projects">PROYECTOS</h2>
+          <br />
+          <p>
+            Mis proyectos más emocionantes y creativos.
+            <span className='span_blue'>
+             Cada proyecto es el
+            resultado de mi dedicación y pasión por la programación.
+            </span> ¡Descubre
+            cómo transformo ideas en realidades digitales!
+          </p>
+          <br />
+          <Projects projectObject={projectObject} />
+        </section>
+        <section className="cont_tecnoloias-padre">
+          <h2 id="tecnologias">TECNOLOGiAS</h2>
+          <p>
+            En mi viaje por el mundo del desarrollo web,
+            <span className="span_yellow">
+              he cultivado experiencia y habilidades en una variedad de
+              tecnologías
+            </span>
+            , Mi stack tecnológico incluye:
+          </p>
+          <br />
+          <Tecnologias iconObject={iconObject} />
+        </section>
+        <section>
+          <h2>Sobre mí</h2>
+          <p>
+            Me llamo Abraham Alfonzo, actualmente estudiante de Ingeniería
+            Informática. Desde que descubrí el mundo de la programación, he
+            estado inmerso en el aprendizaje y la aplicación de diversas
+            tecnologías que me permiten dar vida a mis ideas a través de la web.
+          </p>
+        </section>
+      </article>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- My-Portfolio/src/Components/Projects.jsx
+++ My-Portfolio/src/Components/Projects.jsx
@@ -5,13 +5,25 @@ export function Projects({projectObject}){
             {projectObject.map((element,j)=> {
                 return (
                 <section key={j} className="cards">
+                     <b className="etiqueta-project etiquetas">{element.type}</b>
                     <div className="cont_img_projects">
-                        <b className="etiqueta-project">{element.type}</b>
+                       
                         <img src={element.img} alt="" className="img-cards"/>
                     </div>
+                    <div className="cont_info-cards">
                     <h3>{element.name}</h3>
                     <br />
                     <p>{element.description}</p>
+                    <div style={{'marginTop':'10px'}}>
+                    {element.tecnologia.map((elements,k)=>{
+                        return <b className="etiquetas" style={{'marginTop': '10px'}} key={k}>{elements}</b>
+                    })}
+                    </div>
+                    <div className="cont_a_cards">
+                    <a href="" className="a-cards">Code</a>
+                    <a href="" className="a-cards">Preview</a>
+                    </div>
+                    </div>
                 </section>)
             })}
         </article>
